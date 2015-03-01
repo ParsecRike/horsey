e7ec83f6825e74b0883edf8a4fc5cfd5238298ea
rikiriki <rike@triphoenix.de>
2015-03-01 13:23:03 +0100

    initial commit

---
+++ src/main/java/com/rikinator/controller/RikiTroller.java
@@ -0,0 +1,23 @@
+package com.rikinator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RikiTroller {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "helloworld";
+    }
+
+
+    @RequestMapping("/doit")
+    public String doIt() {
+        return "doit";
+    }
+
+}
